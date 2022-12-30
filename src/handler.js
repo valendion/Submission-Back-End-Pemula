9d63cadbcf1285a12fdc9f45a23cbc0f1e105e48
valendion <valendion874@gmail.com>
2022-12-30 13:33:16 +0800

    Menambahkan delete buku

--- src/handler.js
+++ src/handler.js
@@ -174,9 +174,36 @@ const editBookHandler = (request, h) => {
   }
 };
 
+const deleteBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+  } else {
+    books.splice(index, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+
+    response.code(404);
+    return response;
+  }
+};
+
 module.exports = {
   addBookHandler,
   getAllBooks,
   getBookByIdHandler,
   editBookHandler,
+  deleteBookByIdHandler,
 };
